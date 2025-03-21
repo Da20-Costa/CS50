33de469f1a4e72460216e0d8c0813a8b95f1a4cc
Da20-Costa <da20.sousacosta@gmail.com>
2025-03-21 22:59:58 +0000

    Fri, Mar 21, 2025, 7:59 PM -03:00

---
+++ credit/credit.c
@@ -0,0 +1,22 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <math.h>
+
+int main (void)
+{
+    long credit_card = get_long("Credit Card: ");
+}
+
+int every_other_digit(long credit_card){
+    int sum = 0;
+    bool isAlternateDigit = false;
+    while(credit_card > 0){
+        if(isAlternateDigit == true){
+            int last_digit = credit_card % 10;
+            sum = sum + last_digit;
+        }
+        isAlternateDigit = !isAlternateDigit
+        credit_card = credit_card / 10;
+    }
+    return sum;
+}
