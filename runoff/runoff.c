71dfccb2304acfd8b75b000ad11e1c66453cf62b
Da20-Costa <da20.sousacosta@gmail.com>
2025-05-08 14:24:02 +0000

    Thu, May 8, 2025, 11:24 AM -03:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -174,7 +174,7 @@ bool print_winner(void)
 // Return the minimum number of votes any remaining candidate has
 int find_min(void)
 {
-    // TODO
+    int min_votes = MAX_VOTERS;
     return 0;
 }
 
