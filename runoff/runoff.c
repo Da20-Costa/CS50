4e986ce5503c9400d6081eb29d7b96f179e7c433
Da20-Costa <da20.sousacosta@gmail.com>
2025-05-08 14:18:34 +0000

    Thu, May 8, 2025, 11:18 AM -03:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -159,7 +159,7 @@ void tabulate(void)
 // Print the winner of the election, if there is one
 bool print_winner(void)
 {
-    // TODO
+    int majority = (voter_count / 2) + 1;
     return false;
 }
 
