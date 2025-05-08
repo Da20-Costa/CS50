4f547433100ee2a5c669ccd82b66e58a5dbce1f6
Da20-Costa <da20.sousacosta@gmail.com>
2025-05-08 14:20:47 +0000

    Thu, May 8, 2025, 11:20 AM -03:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -160,6 +160,14 @@ void tabulate(void)
 bool print_winner(void)
 {
     int majority = (voter_count / 2) + 1;
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes >= majority)
+        {
+            printf("%s\n", candidates[i].name);
+            return true;
+        }
+    }
     return false;
 }
 
