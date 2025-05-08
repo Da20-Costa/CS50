f47b7384cab7f4bf063572925487371574aee0ed
Da20-Costa <da20.sousacosta@gmail.com>
2025-05-08 14:26:54 +0000

    Thu, May 8, 2025, 11:26 AM -03:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -175,7 +175,17 @@ bool print_winner(void)
 int find_min(void)
 {
     int min_votes = MAX_VOTERS;
-    return 0;
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidate[i].eliminated == false)
+        {
+            if (candidates[i].votes < min_votes)
+            {
+                min_votes = candidates[i].votes;
+            }
+        }
+    }
+    return min_votes;
 }
 
 // Return true if the election is tied between all candidates, false otherwise
