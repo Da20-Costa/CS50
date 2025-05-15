1f902ba0acb6569b27b4529d3284cd79b6a17fdd
Da20-Costa <da20.sousacosta@gmail.com>
2025-05-15 16:53:01 +0000

    Thu, May 15, 2025, 1:53 PM -03:00

--- recover/recover.c
+++ recover/recover.c
@@ -23,6 +23,9 @@ int main(int argc, char *argv[])
 
     while(fread(buffer, BYTE, BLOCK, card) == BLOCK)
     {
-        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff)
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+        {
+            
+        }
     }
 }
