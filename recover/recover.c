1be6e7f56d7eecbf043c18a269b23132234fbe23
Da20-Costa <da20.sousacosta@gmail.com>
2025-05-15 16:38:01 +0000

    Thu, May 15, 2025, 1:38 PM -03:00

---
+++ recover/recover.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+const int BLOCK = 512;
+typedef uint8_t BYTE;
+
+int main(int argc, char *argv[])
+{
+    if(arg != 2)
+    {
+        printf("Usage: ./recover card.raw\n");
+        return 1;
+    }
+
+    FILE *card = fopen(argv[1], "r");
+    if (input == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
+    BYTE *buffer = malloc(BLOCK * sizeof(BYTE));
+
+    while(fread(buffer, BYTE, BLOCK, card) == BLOCK)
+}
