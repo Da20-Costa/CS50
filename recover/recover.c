73c0d487b4ff3ff2a5352544e948cffce903a3e2
Da20-Costa <da20.sousacosta@gmail.com>
2025-05-15 16:59:20 +0000

    Thu, May 15, 2025, 1:59 PM -03:00

--- recover/recover.c
+++ recover/recover.c
@@ -26,7 +26,11 @@ int main(int argc, char *argv[])
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
             char *image = malloc(7 * sizeof(char));
-            sprintf()
+            for(int i = 0; i < 50; i++)
+            {
+                sprintf(image, "%03i.jpg", i);
+            }
+            
         }
     }
 }
